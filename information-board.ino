a28e591f609c6cc11609f248d1e9a6a792a29ea5
Jakub <bogunia.jakub@gmail.com>
2019-03-14 17:00:17 +0100

    Add AP & AsyncWebServer

--- information-board.ino
+++ information-board.ino
@@ -1,9 +1,19 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <ESPAsyncTCP.h>
+#include <ESPAsyncWebServer.h>
 
+AsyncWebServer server(80);
+
+void setup() {
+
+  WiFi.softAP("Information board");
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+        request->send(200, "text/plain", "{ 'message': 'Super information board XD'}");
+  });
 }
 
+// the loop function runs over and over again forever
 void loop() {
-  // put your main code here, to run repeatedly:
 
 }
