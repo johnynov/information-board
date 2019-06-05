a09ae724d047b2e925f8c09a8c744bc5bc7a0c17
kajkal <karolesniak@gmail.com>
2019-06-05 23:03:31 +0200

    US1: dodawanie ogłoszenia - testy

--- tests/ControllerTest.cpp
+++ tests/ControllerTest.cpp
@@ -3,20 +3,44 @@
 #include "../class/controller/Controller.cpp"
 
 
+Controller controller;
+
 bool is_json_array(const string& s) {
     regex array_regex("\\[.*\\]");
     return regex_match(s, array_regex);
 }
 
-TEST_CASE("Advert controller", "[controller]") {
-    Controller controller;
+bool is_json_advert(const string& s) {
+    regex json_advert("\\{\"id\":.*,\"title\":.*,\"body\":.*,\"password\":.*\\}");
+    return regex_match(s, json_advert);
+}
+
+TEST_CASE("Controller::get_adverts", "[controller]") {
 
     SECTION("should return response object with status 200 and list of adverts") {
         // when
         Response *response = controller.get_adverts();
+        int status = response->getStatus();
+        const string& payload = response->getPayload();
 
         // then
-        REQUIRE(response->getStatus() == 200);
-        REQUIRE(is_json_array(response->getPayload()));
+        REQUIRE(status == 200);
+        REQUIRE(is_json_array(payload));
     }
+
+}
+
+TEST_CASE("Controller::add_advert", "[controller]") {
+
+    SECTION("should return response object with status 200 and added advert") {
+        // when
+        Response *response = controller.add_advert("title", "body", "abcd");
+        int status = response->getStatus();
+        const string& payload = response->getPayload();
+
+        // then
+        REQUIRE(status == 200);
+        REQUIRE(is_json_advert(payload));
+    }
+
 }
--- tests/RepositoryTest.cpp
+++ tests/RepositoryTest.cpp
@@ -13,4 +13,20 @@ TEST_CASE("Advert repository", "[repository]") {
         // then
         REQUIRE(adverts_count == 4);
     }
+
+    SECTION("should add new advert") {
+        // given
+        int initial_adverts_count = repository.find_all().size();
+        string new_advert_id = repository.get_new_id();
+        Advert *new_advert = new Advert(new_advert_id, "Title", "Body", "qwer");
+
+        // when
+        int result = repository.add(new_advert_id, new_advert);
+        int adverts_count = repository.find_all().size();
+
+        // then
+        REQUIRE(result == 1);
+        REQUIRE(adverts_count == initial_adverts_count + 1);
+        REQUIRE(repository.find_by_id(new_advert_id) == new_advert);
+    }
 }
